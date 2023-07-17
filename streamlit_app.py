be3b80b287fafe8cf72bcb02a564163990d0a8c3
Shiela Kristoffersen <46521426+shielakri@users.noreply.github.com>
2023-07-17 14:13:30 +0200

    Update streamlit_app.py
    Fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # Troubleshooting
-streramlit.stop()
+streamlit.stop()
 
 #
 # Connecting to snowflake
